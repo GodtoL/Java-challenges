b46f521fcce52a4403057cf33668584267804c13
GodtoL <eliasvera793@gmail.com>
2024-08-27 19:31:41 -0400

    Fibonacci terminado

---
+++ src/Fibonnacci.java
@@ -0,0 +1,16 @@
+public class Fibonnacci {
+    public static void main(String[] args){
+        long[] numbers = new long[51];
+        numbers[0] = 0;
+        numbers[1] = 1;
+
+        for (int i = 2; i <= 50; i++){
+            numbers[i] = numbers[i-1] + numbers[i-2];
+        }
+
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
