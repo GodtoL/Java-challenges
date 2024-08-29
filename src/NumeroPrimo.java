64e0d3d844926cf081752de0f21396d4c1de31b5
GodtoL <eliasvera793@gmail.com>
2024-08-28 20:04:05 -0400

    Numeros primos en java

--- src/NumeroPrimo.java
+++ src/NumeroPrimo.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class NumeroPrimo {
+public class NumeroPrimo {
+    public static void main(String[] args){
+        for (int i = 2; i < 100; i++){
+            int divCount = 0;
+
+            for (int d = 1; d <= i; d++){
+                if (i % d == 0){
+                    divCount++;
+                }
+            }
+            if (divCount == 2){
+                System.out.println(i);
+            }
+        }
+    }
 }
